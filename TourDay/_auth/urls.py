834209704cb8a841301c5419b5d2ef15d03e21d7
asif <asif.past@gmail.com>
2020-06-04 18:54:26 +0600

    Added Signup page

--- TourDay/_auth/urls.py
+++ TourDay/_auth/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path
-from _auth.views import loginView
+from _auth.views import loginView,signupView
 
 urlpatterns = [
    path('login/',loginView,name='login_page'),
+    path('signup/',signupView,name='signup_page'),
 ]
--- TourDay/_auth/views.py
+++ TourDay/_auth/views.py
@@ -4,3 +4,5 @@ from django.shortcuts import render
 def loginView(request):
     return render(request, "_auth/login.html")
  
+def signupView(request):
+    return render(request, "_auth/signup.html")
