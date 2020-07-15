0089e6e2ea55d441914e6358051f5b07f39771bb
asif <asif.past@gmail.com>
2020-07-16 02:22:09 +0600

    Added Profile Model

--- TourDay/user_profile/admin.py
+++ TourDay/user_profile/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Profile
 # Register your models here.
+
+
+class ProfileConf(admin.ModelAdmin):
+    list_display = ('name', 'user', 'email')
+
+
+admin.site.register(Profile, ProfileConf)
--- TourDay/user_profile/models.py
+++ TourDay/user_profile/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=60, blank=True, null=True)
+    email = models.CharField(max_length=60, blank=True, null=True)
+    fb = models.CharField(max_length=60, blank=True, null=True)
+    bio = models.CharField(max_length=200, blank=True, null=True)
+    city = models.CharField(max_length=30, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        """Check for valid email and also save changed email to User model """
+        if self.email != None:
+            validate_email(self.email)
+            self.user.email = self.email
+            self.user.save()
+        super().save(*args, **kwargs)
