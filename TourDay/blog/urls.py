d1342956ad6876a45295ff27f4c04563970441d2
pkbagchi <parthabagchi249@gmail.com>
2020-07-18 13:19:13 +0600

    Add blog home page

--- TourDay/blog/urls.py
+++ TourDay/blog/urls.py
@@ -1,11 +1,12 @@
 
 from django.urls import path
-from blog.views import search
+from blog.views import search, home
 
 
 
 
 urlpatterns = [
-  path("",search,name="search_page")
+  path("",search,name="search_page"),
+  path('blog/home', home, name='home' ),
    
 ]
--- TourDay/blog/views.py
+++ TourDay/blog/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def search(request):
     return render(request,'blog/search.html')
+
+def home(request):
+    return render(request,'blog/home.html')
