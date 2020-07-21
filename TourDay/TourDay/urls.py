954c4723b2571d01f0d3e705999d38752def8c29
pkbagchi <parthabagchi249@gmail.com>
2020-07-21 19:37:22 +0600

    Fixed Upload Image on editor

--- TourDay/TourDay/urls.py
+++ TourDay/TourDay/urls.py
@@ -4,12 +4,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('_auth.urls')),
     path('', include('blog.urls')),
     path('', include('user_profile.urls')),
-    path('ckeditor/', include('ckeditor_uploader.urls')),
+    # path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- TourDay/blog/urls.py
+++ TourDay/blog/urls.py
@@ -1,13 +1,19 @@
 
 from django.urls import path
 from blog.views import search, home, details, addPost
-
-
+from ckeditor_uploader import views as uploader_views
+from django.views.decorators.cache import never_cache
 
 
 urlpatterns = [
+
+  path('ckeditor/upload/', uploader_views.upload, name='ckeditor_upload'),
+  path('ckeditor/browse/', never_cache(uploader_views.browse), name='ckeditor_browse'),
+
+
   path("",search,name="search_page"),
   path('blog/home', home, name='blog_home' ),
   path('blog/details/<int:id>', details, name='blog_details' ),
   path('blog/addpost', addPost, name='addpost' ),
+ 
 ]
