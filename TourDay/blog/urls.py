d541b9869932312ff8e73ca525364bf2059cda4f
pkbagchi <parthabagchi249@gmail.com>
2020-08-01 09:54:27 +0600

    Add: division catagories

--- TourDay/blog/urls.py
+++ TourDay/blog/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from blog.views import search, home, addPost, details, user_post
+from blog.views import search, home, addPost, details, user_post, blog_edit, blog_delete, division_post
 
 from ckeditor_uploader import views as uploader_views
 from django.views.decorators.cache import never_cache
@@ -16,8 +16,9 @@ urlpatterns = [
   path('blog/home', home, name='blog_home' ),
   path('blog/details/<int:id>', details, name='blog_details' ),
   path('blog/addpost', addPost, name='addpost' ),
-  # path('blog/edit/<int:id>', blog_edit, name='blog_edit' ),
-  # path('blog/delete/<int:id>', blog_delete, name='blog_delete' ),
+  path('blog/edit/<int:id>', blog_edit, name='blog_edit' ),
+  path('blog/delete/<int:id>', blog_delete, name='blog_delete' ),
   path('blog/user/<slug>', user_post, name='user_post' ),
+  path('blog/division/<slug>', division_post, name='division_post'),
   
 ]
--- TourDay/blog/views.py
+++ TourDay/blog/views.py
@@ -62,47 +62,72 @@ def addPost(request):
     return render(request,'blog/add_post.html', {'form':form})
 
 
-# @login_required
-# def blog_edit(request, id):
+@login_required
+def blog_edit(request, id):
     
-#     post = blogPost.objects.get(id=id)
+    post = blogPost.objects.get(id=id)
 
-#     if request.user == post.blog_user:
-#         item = get_object_or_404(blogPost, id=id)
-#         form = blogPostForm(request.POST or None,request.FILES or None, instance=item)
+    if request.user == post.blog_user:
+        item = get_object_or_404(blogPost, id=id)
+        form = blogPostForm(request.POST or None,request.FILES or None, instance=item)
 
-#         if form.is_valid():
-#             obj= form.save(commit= False)
-#             obj.save()
+        if form.is_valid():
+            obj= form.save(commit= False)
+            obj.save()
 
-#             return redirect('blog_home')
-#     else:
-#         pass
+            return redirect('blog_details', id)
+    else:
+        return redirect('blog_details', id)
 
     
-#     return render(request, 'blog/add_post.html', {'form' : form})
+    return render(request, 'blog/add_post.html', {'form' : form})
 
 
-# @login_required
-# def blog_delete(request, id):
+@login_required
+def blog_delete(request, id):
     
-#     post = blogPost.objects.get(id=id)
+    post = blogPost.objects.get(id=id)
 
-#     if request.user == post.blog_user:
-#         item = get_object_or_404(blogPost, id=id)
-#         item.delete()
+    if request.user == post.blog_user:
+        item = get_object_or_404(blogPost, id=id)
+        item.delete()
 
-#         return redirect('blog_home')
-#     else:
-#         pass
+        return redirect('user_post', request.user)
+    else:
+        return redirect('user_post', request.user)
 
-#     return render(request, 'blog/delete.html', {'post' : post})
+    return render(request, 'blog/delete.html', {'post' : post})
 
 
 def user_post(request, slug):
     
     post1 = blogPost.objects.filter(slug=slug).order_by('id')
     post = blogPost.objects.filter(slug=slug).order_by('-id')
+    paginator = Paginator(post, 5)  # Show 10 obj per page
+    random_post = blogPost.objects.order_by('?')
+
+    page = request.GET.get('page')
+    post = paginator.get_page(page)
+
+    context = {
+        'post':post, 
+        'post1':post1,
+        'random_post' : random_post,
+    }
+
+    # post = get_object_or_404(blogPost, blog_user=request.user)
+    # print(post)
+
+    # post = blogPost.objects.get(blog_user=request.user)
+
+    return render(request, 'blog/user_post.html', context)
+
+
+
+def division_post(request, slug):
+
+    post1 = blogPost.objects.filter(division=slug).order_by('id')
+    post = blogPost.objects.filter(division=slug).order_by('-id')
     paginator = Paginator(post, 5)  # Show 10 obj per page
     random_post = blogPost.objects.order_by('?')
 
