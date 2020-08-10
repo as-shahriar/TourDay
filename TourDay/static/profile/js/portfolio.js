9116daae3d965ee355d5359305598fb1f1e82d58
asif <asif.past@gmail.com>
2020-08-10 23:49:21 +0600

    Added Create Post Frontend

---
+++ TourDay/static/profile/js/portfolio.js
@@ -0,0 +1,51 @@
+select_picture = document.getElementById("picture");
+pic_btn = document.getElementById("pic-btn");
+preview_div = document.getElementById("preview");
+preview_close = document.getElementById("preview-close");
+pic_preview = document.getElementById("pic_preview");
+
+select_picture.addEventListener("change", () => {
+  const file = select_picture.files[0];
+  if (!isFileImage(file)) {
+    select_picture.value = "";
+    return;
+  }
+  previewFile(file);
+  pic_btn.style.display = "none";
+
+  preview_div.style.display = "block";
+});
+
+preview_close.addEventListener("click", () => {
+  pic_btn.style.display = "block";
+  preview_div.style.display = "none";
+  select_picture.value = "";
+});
+
+function previewFile(file) {
+  const reader = new FileReader();
+
+  reader.addEventListener(
+    "load",
+    function () {
+      // convert image file to base64 string
+      pic_preview.src = reader.result;
+    },
+    false
+  );
+
+  if (file) {
+    reader.readAsDataURL(file);
+  }
+}
+
+function isFileImage(file) {
+  const acceptedImageTypes = [
+    "image/gif",
+    "image/jpeg",
+    "image/png",
+    "image/webp",
+  ];
+
+  return file && acceptedImageTypes.includes(file["type"]);
+}
