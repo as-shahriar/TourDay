5d7747617092f681220ea05546e1ef2f6ff2d4d9
Asif Shahriar <a.shahriar1999@gmail.com>
2020-09-16 22:33:16 +0600

    Fixed CI error

--- TourDay/TourDay/settings.py
+++ TourDay/TourDay/settings.py
@@ -85,17 +85,22 @@ WSGI_APPLICATION = "TourDay.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'tourdaydb',
+#         'USER': 'dbuser',
+#         'PASSWORD': '123456',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'tourdaydb',
-        'USER': 'dbuser',
-        'PASSWORD': '123456',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
