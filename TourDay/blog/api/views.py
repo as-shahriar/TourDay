a4dd211c8696ef54a8c8306dfc6874a16af48554
pkbagchi <parthabagchi249@gmail.com>
2020-10-05 10:07:57 +0600

    Add paginator class

--- TourDay/blog/api/views.py
+++ TourDay/blog/api/views.py
@@ -144,7 +144,7 @@ def api_blogDelete(request, id):
 
 
 class SetPagination(PageNumberPagination):
-    page_size = 2
+    page_size = 7
     page_size_query_param = 'page_size'
     max_page_size = 10000
 
