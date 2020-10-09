cc4638f6a8774f4e31a2454accabd57867852a05
asifo1 <a.shahriar1999@gmail.com>
2020-10-09 21:05:29 +0600

    Fixed post image problem
    add random image name to resolve the issue

--- TourDay/utils.py
+++ TourDay/utils.py
@@ -1,89 +1,92 @@
+import string
+import random
 import threading
 from django.core.mail import send_mail
 from time import sleep
 
 districts = {
 
-        # Rangpur
-        "Panchagarh": "1",
-        "Thakurgaon": "2",
-        "Dinajpur": "4",
-        "Nilphamari": "0",
-        "Lalmonirhat": "3",
-        "Kurigram": "6",
-        "Rangpur": "5",
-        "Gaibandha": "7",
+    # Rangpur
+    "Panchagarh": "1",
+    "Thakurgaon": "2",
+    "Dinajpur": "4",
+    "Nilphamari": "0",
+    "Lalmonirhat": "3",
+    "Kurigram": "6",
+    "Rangpur": "5",
+    "Gaibandha": "7",
 
-        # Rajshahi
-        "Joypurhat": "16",
-        "Bogra": "11",
-        "Naogaon": "15",
-        "Natore": "12",
-        "Chapainawabganj": "10",
-        "Pabna": "9",
-        "Rajshahi": "13",
-        "Sirajganj": "14",
+    # Rajshahi
+    "Joypurhat": "16",
+    "Bogra": "11",
+    "Naogaon": "15",
+    "Natore": "12",
+    "Chapainawabganj": "10",
+    "Pabna": "9",
+    "Rajshahi": "13",
+    "Sirajganj": "14",
 
-        # Mymensingh
-        "Jamalpur": "66",
-        "Mymensingh": "67",
-        "Netrokona ": "68",
-        "Sherpur": "66a",
+    # Mymensingh
+    "Jamalpur": "66",
+    "Mymensingh": "67",
+    "Netrokona ": "68",
+    "Sherpur": "66a",
 
-        # Sylhet
-        "Habiganj": "64",
-        "Moulvibazar": "65",
-        "Sunamganj": "62",
-        "Sylhet": "63",
+    # Sylhet
+    "Habiganj": "64",
+    "Moulvibazar": "65",
+    "Sunamganj": "62",
+    "Sylhet": "63",
 
-        # Dhaka
-        "Dhaka": "73",
-        "Faridpur": "70",
-        "Gazipur": "76",
-        "Gopalganj": "71",
-        "Kishoreganj": "83",
-        "Madaripur": "75",
-        "Manikganj": "72",
-        "Munshiganj": "79",
-        "Narayanganj": "78",
-        "Narsingdi": "77",
-        "Rajbari": "69",
-        "Shariatpur": "74",
-        "Tangail": "84",
+    # Dhaka
+    "Dhaka": "73",
+    "Faridpur": "70",
+    "Gazipur": "76",
+    "Gopalganj": "71",
+    "Kishoreganj": "83",
+    "Madaripur": "75",
+    "Manikganj": "72",
+    "Munshiganj": "79",
+    "Narayanganj": "78",
+    "Narsingdi": "77",
+    "Rajbari": "69",
+    "Shariatpur": "74",
+    "Tangail": "84",
 
-        # Khulna
-        "Jessore": "19",
-        "Jhenaidah": "17",
-        "Khulna": "22",
-        "Kushtia": "23",
-        "Magura": "18",
-        "Meherpur": "24",
-        "Narail": "20",
-        "Satkhira": "21",
-        "Bagerhat": "26",
-        "Chuadanga": "25",
+    # Khulna
+    "Jessore": "19",
+    "Jhenaidah": "17",
+    "Khulna": "22",
+    "Kushtia": "23",
+    "Magura": "18",
+    "Meherpur": "24",
+    "Narail": "20",
+    "Satkhira": "21",
+    "Bagerhat": "26",
+    "Chuadanga": "25",
 
-        # Barisal
-        "Barguna": "32",
-        "Barisal": "31",
-        "Bhola": "37",
-        "Jhalokati": "30",
-        "Patuakhali": "33",
-        "Pirojpur": "29",
+    # Barisal
+    "Barguna": "32",
+    "Barisal": "31",
+    "Bhola": "37",
+    "Jhalokati": "30",
+    "Patuakhali": "33",
+    "Pirojpur": "29",
 
-        # Chattogram
-        "Chattogram": "54",
-        "Bandarban": "56",
-        "Brahmanbaria": "41",
-        "Chandpur": "44",
-        "Comilla": "43",
-        "Cox's Bazar": "59",
-        "Feni": "51",
-        "Khagrachhari": "55",
-        "Lakshmipur": "45",
-        "Noakhali": "46",
-        "Rangamati": "57",
-    }
+    # Chattogram
+    "Chattogram": "54",
+    "Bandarban": "56",
+    "Brahmanbaria": "41",
+    "Chandpur": "44",
+    "Comilla": "43",
+    "Cox's Bazar": "59",
+    "Feni": "51",
+    "Khagrachhari": "55",
+    "Lakshmipur": "45",
+    "Noakhali": "46",
+    "Rangamati": "57",
+}
+
 
 def threading_send_mail(subject, message, EMAIL_HOST_USER, user_email):
     send_mail(subject, message, EMAIL_HOST_USER,
@@ -97,8 +100,12 @@ def async_send_mail(subject, message, EMAIL_HOST_USER, user_email):
     thread.start()
 
 
-
-
 def number_to_location(number):
     """Take number and return location"""
     return list(districts.keys())[list(districts.values()).index(number)]
+
+
+def get_random_string(length):
+    letters = string.ascii_lowercase
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
