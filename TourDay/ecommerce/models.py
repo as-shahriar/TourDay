83f797ffb102350875f840323810f34cf6ce9fab
asifo1 <a.shahriar1999@gmail.com>
2020-10-20 15:34:58 +0600

    Created modal view for product

--- TourDay/ecommerce/models.py
+++ TourDay/ecommerce/models.py
@@ -18,10 +18,10 @@ STATUS_CHOICES = (
 
 class Product(models.Model):
     name = models.CharField(max_length=30, blank=True)
-    discription = models.CharField(max_length=250,null=True, blank=True)
-    product_type = models.CharField(max_length=20,null=True, blank=True)
-    price = models.IntegerField()
-    digital = models.BooleanField(default=False,null=True, blank=True)
+    discription = models.CharField(max_length=250, null=True, blank=True)
+    product_type = models.CharField(max_length=20, null=True, blank=True)
+    price = models.PositiveIntegerField()
+    digital = models.BooleanField(default=False, null=True, blank=True)
     image = models.ImageField(upload_to='ecom_pics', blank=False)
 
     def save(self):
@@ -38,19 +38,22 @@ class Product(models.Model):
             url = ''
         return url
 
+
 class Order(models.Model):
-    customer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    customer = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, blank=True)
     date_ordered = models.DateField(default=datetime.date.today)
     complete = models.BooleanField(default=False)
 
     total_money = models.IntegerField(default=0, null=True, blank=True)
     total_items = models.IntegerField(default=0, null=True, blank=True)
-    status = models.CharField(max_length=30,null=True, blank=True,choices=STATUS_CHOICES, default="Pending")
+    status = models.CharField(
+        max_length=30, null=True, blank=True, choices=STATUS_CHOICES, default="Pending")
     order_id = models.CharField(max_length=100, null=True)
 
     def __str__(self):
         return self.order_id
-		
+
     @property
     def shipping(self):
         shipping = False
@@ -64,24 +67,26 @@ class Order(models.Model):
     def get_cart_total(self):
         orderitems = self.orderitem_set.all()
         total = sum([item.get_total for item in orderitems])
-        return total 
+        return total
 
     @property
     def get_cart_items(self):
         orderitems = self.orderitem_set.all()
         total = sum([item.quantity for item in orderitems])
-        return total 
+        return total
+
 
 class OrderItem(models.Model):
-	product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
-	order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
-	quantity = models.IntegerField(default=0, null=True, blank=True)
-	date_added = models.DateField(default=datetime.date.today)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateField(default=datetime.date.today)
 
-	@property
-	def get_total(self):
-		total = self.product.price * self.quantity
-		return total
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
+
 
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
@@ -97,18 +102,20 @@ class ShippingAddress(models.Model):
     def __str__(self):
         return self.address
 
+
 class payment(models.Model):
     customer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
-    
-    method = models.CharField(max_length=50,null=True, blank=True)
-    
-    payment_method = models.CharField(max_length=50,null=True, blank=True)
+
+    method = models.CharField(max_length=50, null=True, blank=True)
+
+    payment_method = models.CharField(max_length=50, null=True, blank=True)
     PhoneNo = models.CharField(max_length=50, null=False, blank=True)
     trxId = models.CharField(max_length=50, null=False, blank=True)
 
     def __str__(self):
         return str(self.customer)
+
 
 class Product_type(models.Model):
     product_type = models.CharField(max_length=50, null=False, blank=True)
