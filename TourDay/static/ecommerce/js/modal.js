2cd7dd272edf29b911342d86daaff6ce8dc1768b
asifo1 <a.shahriar1999@gmail.com>
2020-10-21 00:12:13 +0600

    Added ads redirect other page to modal view

--- TourDay/static/ecommerce/js/modal.js
+++ TourDay/static/ecommerce/js/modal.js
@@ -2,13 +2,7 @@
 document.querySelectorAll(".open-modal").forEach(e=>{
     e.addEventListener("click",()=>{
         id = e.getAttribute("data-id")
-        fetch(`/api/ecommerce/product/details/${id}`).
-        then(res=>res.json()).
-        then(data=>{
-            const {discription, image,name,price} = data
-            changeModal(id,name,discription,price,image)
-            document.getElementById("modal-triger").click();
-        })
+        get_product_info(id)
     })
 })
 
@@ -17,4 +11,22 @@ changeModal = (id,title,description,price,img)=>{
     document.getElementById("modal-title").textContent = title
     document.getElementById("modal-description").textContent = description
     document.getElementById("modal-price").textContent = `৳${price}`
+}
+
+get_product_info = (id)=>{
+    fetch(`/api/ecommerce/product/details/${id}`).
+    then(res=>res.json()).
+    then(data=>{
+        const {discription, image,name,price} = data
+        changeModal(id,name,discription,price,image)
+        document.getElementById("modal-triger").click();
+    })
+}
+
+url = window.location.href;
+re = /#[0-9]+/
+value_in_url = re.exec(url)
+if (value_in_url){
+    id = value_in_url[0].replace(/#/,"")
+    get_product_info(id)
 }
