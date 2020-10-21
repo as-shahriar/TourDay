fbe4b0980bfa9065beec3f844fe6c76161ccff3c
pkbagchi <parthabagchi249@gmail.com>
2020-10-21 18:29:38 +0600

    Added availibility status

--- TourDay/static/ecommerce/js/modal.js
+++ TourDay/static/ecommerce/js/modal.js
@@ -5,16 +5,25 @@ document.querySelectorAll(".open-modal").forEach(e=>{
         fetch(`/api/ecommerce/product/details/${id}`).
         then(res=>res.json()).
         then(data=>{
-            const {description, image,name,price} = data
-            changeModal(id,name,description,price,image)
+            console.log(data)
+            const {description, image,name,price,product_type,digital} = data
+            changeModal(id,name,description,price,image,product_type,digital)
             document.getElementById("modal-triger").click();
         })
     })
 })
 
-changeModal = (id,title,description,price,img)=>{
+changeModal = (id,title,description,price,img,product_type,digital)=>{
     document.getElementById("modal-img").src = img
     document.getElementById("modal-title").textContent = title
-    document.getElementById("modal-description").textContent = description
     document.getElementById("modal-price").textContent = `৳${price}`
+    document.getElementById("modal-product_type").textContent = `Category : ${product_type}`
+    if(digital === false) {
+        document.getElementById("modal-status").textContent = 'Availibility : In Stock'
+    }
+    else {
+        document.getElementById("modal-status").textContent = 'Availibility : Out Stock'
+    }
+    document.getElementById("modal-description").textContent = description
+    
 }
