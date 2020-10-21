d588fbb14d164be14f5319cd8e8cfcf0ce65c29b
pkbagchi <parthabagchi249@gmail.com>
2020-10-21 19:16:24 +0600

    Updated ads_engine pages

--- TourDay/ecommerce/ads_engine.py
+++ TourDay/ecommerce/ads_engine.py
@@ -2,7 +2,7 @@ from ecommerce.models import Product
 
 
 def get_ads():
-    objects = Product.objects.filter(in_stock=True).order_by('?')
+    objects = Product.objects.filter(digital=False).order_by('?')
     if objects.count() > 5:
         return objects
     else:
