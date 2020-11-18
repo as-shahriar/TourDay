c13a4c049ccb5c135a85d6d912f269f7b5ac5f30
asifo1 <a.shahriar1999@gmail.com>
2020-11-18 20:55:34 +0600

    Added search api endpoints

--- TourDay/api/urls.py
+++ TourDay/api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 from api import auth, profile, views, event
 from _auth.views import forgetPasswordView, resetPasswordView
-from user_profile.views import PostList,delete_account
+from user_profile.views import PostList, delete_account
 from django.views.generic import TemplateView
-from event.views import AllEventList,pay,dashboard,edit_event
+from event.views import AllEventList, pay, dashboard, edit_event
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name="api/doc.html"), name="api_doc"),
@@ -13,7 +13,7 @@ urlpatterns = [
     path('auth/signup/', auth.Signup.as_view()),
     path('forget_password/', forgetPasswordView),
     path('reset_password/<str:slug>/', resetPasswordView),
-    path('delete_account/',delete_account),
+    path('delete_account/', delete_account),
 
     # Profile
     path('profile/', profile.ProfileView.as_view()),
@@ -27,14 +27,15 @@ urlpatterns = [
 
     # Event
     path('get_events/<str:username>', event.EventListApi.as_view()),
-    path('all-events/',AllEventList.as_view()),
-    path('going-events/<str:username>',event.GoingEventList.as_view()),
-    path("event-pay/<int:id>",pay),
-    path('create_event/',dashboard),
-    path('edit_event/<int:id>',edit_event),
-    path('going_users/<str:id>',event.GoingUser.as_view()),
+    path('all-events/', AllEventList.as_view()),
+    path('going-events/<str:username>', event.GoingEventList.as_view()),
+    path("event-pay/<int:id>", pay),
+    path('create_event/', dashboard),
+    path('edit_event/<int:id>', edit_event),
+    path('going_users/<str:id>', event.GoingUser.as_view()),
 
-
+    path('search/user/<str:q>', views.SearchUser.as_view()),
+    path('search/product/<str:q>', views.SearchProduct.as_view()),
     path('map/<str:username>', views.map),
 
 ]
